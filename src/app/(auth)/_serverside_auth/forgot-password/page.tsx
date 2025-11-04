2f673e7d9e9a6aa641fa639cd42d56ee5e34b35b
wagnerjw <julian@wagnerjw.dev>
2025-11-03 18:06:36 -0800

    build: continue userland cleanup

--- src/app/(auth)/_serverside_auth/forgot-password/page.tsx
+++ src/app/(auth)/_serverside_auth/forgot-password/page.tsx
@@ -2,7 +2,7 @@ import {
   FormMessage,
   Message,
 } from '@/app/(auth)/components/supabase_auth/form-message';
-import { SubmitButton } from '@/app/(private)/[username]/components/submit-button';
+import { SubmitButton } from '@/app/(auth)/components/supabase_auth/submit-button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { forgotPasswordAction } from './actions';
